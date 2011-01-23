d89373e8b679997b10e66318840e4ad4f7307fc1
Kenneth Reitz <me@kennethreitz.com>
2011-01-23 12:25:49 -0500

    test authentication model

--- mead/core/models/auth.py
+++ mead/core/models/auth.py
@@ -1,9 +1,25 @@
 from flaskext.sqlalchemy import SQLAlchemy
-from mead_core import app
+from mead import app
 
 __version__ = '0.0.0'
 
 db = SQLAlchemy(app)
 
 
+class Test(db.Model):
+	"""The :class:`Test` object is a test of the app structure.
+	"""
+	__tablename__ = 'tests'
 
+	id = db.Column(db.Integer, primary_key=True)
+	value = db.Column(db.Text)
+
+	def __init__(self, value):
+		self.value = value
+
+	def __repr__(self):
+		return '<Test #%r>' % self.id
+
+
+# meta class page
+# meta class post
