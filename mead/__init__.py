f4f8a43c1cea92fb23e9ebf51cb6d9b159bc217d
Kenneth Reitz <me@kennethreitz.com>
2011-01-23 12:15:07 -0500

    mead's true core has been born

---
+++ mead/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    mead.core
+    ~~~~~~~~~~~~
+
+    Mead's core. The Honey.
+
+    :copyright: (c) 2011 Kenneth Reitz
+    :license: ISC
+    :developer: Kenneth Reit
+
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# CONFIGURATION
+app.config.from_object('mead.core.defaults')
+try:
+	# load from local environment.
+	app.config.from_envvar('MEAD_SETTINGS')
+except RuntimeError:
+	pass
+
+
+# Configure SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE']
+del app.config['DATABASE']
+
+
+# Views
+import mead.core.views.content
+import mead.core.admin.views
+
+if __name__ == '__main__':
+	app.run()
