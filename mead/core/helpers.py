f8aaba810b837295c6b2e752e928150616b8f6b2
Kenneth Reitz <me@kennethreitz.com>
2011-01-23 16:21:33 -0500

    Coming together.

--- mead/core/helpers.py
+++ mead/core/helpers.py
@@ -1,14 +1,8 @@
 from mead import app
 from flask import g
-from flaskext.themes import static_file_url, render_theme_template, get_theme
+from flaskext.themes import get_theme
 
 
 @app.before_request
 def get_my_theme():
-	g.theme = get_theme('default')
-	print g.theme.__dict__
-
-#print app.theme_manager.themes
-#print 'fuck'
-
-#print get_theme('default')
+	g.theme = get_theme(app.config['THEME'])
