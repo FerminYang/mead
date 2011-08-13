9e85d9e8a4d4a3e792aa4914cf58eddf186d2ae8
Hank Gay <hank.gay@gmail.com>
2011-08-13 17:01:01 -0400

    Fix compile errors so I can use run_server.py.
    Just some missing imports and variable definitions. I also
    removed/commented some unused imports.

--- mead/core/views/available.py
+++ mead/core/views/available.py
@@ -1,16 +1,19 @@
-from flask import url_for
-from mead import app
+from sqlalchemy.exc import OperationalError
+#from flask import url_for
+from flaskext.sqlalchemy import SQLAlchemy
 
-from mead.core.models.content import Page, Post
+from mead import app
+from mead.core.models.content import Page
+
 
 app.test_request_context('/').push()
+
+db = SQLAlchemy()
 
 defaults = dict(
         site_title = app.config['SITE_TITLE'],
         site_root_url = '/',
         static_url = app.config['THEME'],
-#
-
 #       url_for('.themes/', filename='style.css')
 )
 
