dc78c5d79745edfa721b7315347ce72d9a82cebd
Hank Gay <hank.gay@gmail.com>
2011-08-13 11:18:20 -0400

    Remove hardtabs.

--- setup.py
+++ setup.py
@@ -8,39 +8,39 @@ from distutils.core import setup
 
 
 def publish():
-	"""Publish to PyPi"""
-	os.system("python setup.py sdist upload")
+    """Publish to PyPi"""
+    os.system("python setup.py sdist upload")
 
 if sys.argv[-1] == "publish":
-	publish()
-	sys.exit()
+    publish()
+    sys.exit()
 
 required = []
 
 setup(
-	name='mead',
-	version='0.0.0',
-	description='[Work In Progress] Flask-based CMS Influenced by WordPress',
-	long_description=open('README.rst').read() + '\n\n' +
-	                 open('HISTORY.rst').read(),
-	author='Kenneth Reitz',
-	author_email='me@kennethreitz.com',
-	url='https://github.com/kennethreitz/mead',
-	packages= [
-		'mead',
-	],
-	install_requires=required,
-	license='MIT',
-	classifiers=(
-		'Development Status :: 1 - Planning',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'License :: OSI Approved :: ISC License (ISCL)',
-		'Programming Language :: Python',
+    name='mead',
+    version='0.0.0',
+    description='[Work In Progress] Flask-based CMS Influenced by WordPress',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/mead',
+    packages= [
+        'mead',
+    ],
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
         # 'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7',
-		# 'Programming Language :: Python :: 3.0',
-		# 'Programming Language :: Python :: 3.1',
-	),
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
 )
