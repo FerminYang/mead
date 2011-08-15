c85dfbf9d2a48d9459d21b0b48a07786ebbb2ce4
Hank Gay <hank.gay@gmail.com>
2011-08-15 00:30:44 -0400

    Clean up imports.

--- mead/__init__.py
+++ mead/__init__.py
@@ -9,20 +9,15 @@
     :license: ISC
     :developer: Kenneth Reitz
 """
-
 import os
-
-
-from flask import Flask, g
+from flask import Flask
+from flaskext.themes import setup_themes
 from werkzeug import cached_property
 from jinja2 import ChoiceLoader, FileSystemLoader
 
-from flaskext.themes import setup_themes
-
 
 class MeadFlask(Flask):
-    """ Enhanced Flask class for smart mead template loading
-    """
+    """Enhanced Flask class for smart mead template loading."""
     @cached_property
     def jinja_loader(self):
         return ChoiceLoader([
@@ -41,40 +36,27 @@ setup_themes(app)
 # Load Mead defaults.
 app.config.from_object('mead.core.defaults')
 
-
 try:
     app.config.from_envvar('MEAD_SETTINGS') # Change if you have multiple instances.
 except RuntimeError:
     pass
 
-
 try:
     # Load from local environment.
     app.config.from_pyfile('local_config.py')
-except RuntimeError:
+except IOError:
+    # To paraphrase Drago: If it errors out, it errors out.
     pass
-
 
 # Debug?
 #if os.uname()[1] in app.config['DEBUG_HOSTS']:
 if 1:
     app.config.debug = True
 
-
 # Configure SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE']
 del app.config['DATABASE']
 
-
-
-# =====
-# VIEWS
-# =====
-
-import mead.core.views.content
-import mead.core.admin.views
-
-import mead.core.helpers
 
 if __name__ == '__main__':
     app.run()
